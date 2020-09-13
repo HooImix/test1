6dbcf9ca94875b22a95e3c5737c4b0910da0985c
HooImix <41872503+HooImix@users.noreply.github.com>
2020-09-13 09:05:00 -0700

    adding test file

---
+++ AdvancedFunctions.js
@@ -0,0 +1,46 @@
+//////////
+// AVANCED FUNCTIONS
+/////////
+
+//pre:
+function first() {
+    var greet = 'Hi';
+    function second() {
+        alert(greet);
+    }
+    return second;
+}
+
+//post:
+const first = () => {
+    const greet = 'Hi';
+    const second = () => {
+        alert(greet);
+    }
+    return second;
+}
+
+const newFunc = first();
+newFunc();
+
+//Closures: - a function ran. the function executed. It's never going to exectute again. But it's going to remember that there are references to those variables, so the child scope always has access to the parent scope.
+
+
+
+
+
+
+//Curring
+const multiply = (a,b) => a * b;
+const curriedMultiply = (a) => (b) => a * b;
+const multiplyBy5 = curriedMultiply(5);
+curriedMultiply(3);
+
+//Compose
+const compose = (f, g) => (a) => f(g(a));
+const sum = (num) => num + 1;
+compose(sum, sum)(5);
+
+
+
+//Avoiding side-effects and functional purity
